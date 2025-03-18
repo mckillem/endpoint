1617563f150e9014c8a261d7ba3952c300033b04
Matěj <mckillem@tuta.io>
2025-03-18 09:56:35 +0100

    init

---
+++ temp/cache/nette.search/a53e811bba0f03d98562d7ad76c81016.php
@@ -0,0 +1,42 @@
+<?php
+return array (
+  0 => 
+  array (
+    'App\\Core\\RouterFactory' => 
+    array (
+      0 => '/var/www/html/app/Core/RouterFactory.php',
+      1 => 1738539838,
+    ),
+    'App\\Bootstrap' => 
+    array (
+      0 => '/var/www/html/app/Bootstrap.php',
+      1 => 1738539838,
+    ),
+    'App\\Presentation\\Home\\HomePresenter' => 
+    array (
+      0 => '/var/www/html/app/Presentation/Home/HomePresenter.php',
+      1 => 1738539838,
+    ),
+    'App\\Presentation\\Accessory\\LatteExtension' => 
+    array (
+      0 => '/var/www/html/app/Presentation/Accessory/LatteExtension.php',
+      1 => 1738539838,
+    ),
+    'App\\Presentation\\Error\\Error4xx\\Error4xxPresenter' => 
+    array (
+      0 => '/var/www/html/app/Presentation/Error/Error4xx/Error4xxPresenter.php',
+      1 => 1738539838,
+    ),
+    'App\\Presentation\\Error\\Error5xx\\Error5xxPresenter' => 
+    array (
+      0 => '/var/www/html/app/Presentation/Error/Error5xx/Error5xxPresenter.php',
+      1 => 1738539838,
+    ),
+  ),
+  1 => 
+  array (
+  ),
+  2 => 
+  array (
+  ),
+);
