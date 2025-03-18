7c33516d49bfbdbbd097788d9623da10e4f8d423
Matěj <mckillem@tuta.io>
2025-03-18 11:46:18 +0100

    načítání položek u objednávky

--- .docker/build/database/01_create.sql
+++ .docker/build/database/01_create.sql
@@ -10,52 +10,34 @@ CREATE TABLE `order`
   CHARSET = utf8;
 
 INSERT INTO `order` (`id`, `createdAt`, `title`, `price`, `currency`, `state`)
-VALUES (1, '2024-02-28 12:43:15', 'Název objednávky', 1000, 'Kč', 'Hotovo');
+VALUES (1, '2024-02-28 12:43:15', 'Název objednávky', 1800, 'Kč', 'Hotovo');
 
-# CREATE TABLE `author`
-# (
-#        `id` int(11) NOT NULL AUTO_INCREMENT,
-#        `name` varchar(255) NOT NULL,
-#        PRIMARY KEY (`id`)
-# ) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
-#
-# INSERT INTO `author` (`id`, `name`)
-# VALUES (1,	'Ľuboslav Lacko'),
-#        (2,	'Anna Havlíčková'),
-#        (3,	'Petr Roudenský'),
-#        (4,	'Stephen Prata');
-#
-# CREATE TABLE `book_author`
-# (
-#         `id` int(11) NOT NULL AUTO_INCREMENT,
-#          `book_id` int(11) NOT NULL,
-#          `author_id` int(11) NOT NULL,
-#          PRIMARY KEY (`id`),
-#          KEY `book_id` (`book_id`),
-#          KEY `author_id` (`author_id`),
-#          CONSTRAINT `book_author_ibfk_11` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-#          CONSTRAINT `book_author_ibfk_10` FOREIGN KEY (`book_id`) REFERENCES `order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-# ) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
-#
-# INSERT INTO `book_author` (`id`, `book_id`, `author_id`)
-# VALUES (20,	1,	2),
-#        (21,	2,	3),
-#        (22,	3,	1),
-#        (23,	4,	4),
-#        (24,	5,	2);
-#
-# CREATE TABLE IF NOT EXISTS `user` (
-#                                       `id` int(11) NOT NULL AUTO_INCREMENT,
-#                                       `firstname` varchar(255) COLLATE utf8_czech_ci NOT NULL,
-#                                       `lastname` varchar(255) COLLATE utf8_czech_ci NOT NULL,
-#                                       `email` varchar(255) COLLATE utf8_czech_ci NOT NULL,
-#                                       `password` varchar(60) COLLATE utf8_czech_ci NOT NULL,
-#                                       `role` enum('member','admin') COLLATE utf8_czech_ci NOT NULL DEFAULT 'member',
-#                                       PRIMARY KEY (`id`),
-#                                       UNIQUE KEY `email` (`email`) USING BTREE
-# ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
-#
-# # login cms@itnetwork.cz
-# # heslo itnetwork
-# INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `password`, `role`) VALUES
-#     (1, 'it', 'network', 'cms@itnetwork.cz', '$2y$10$arJIJie/xGoqZayCro4yZ.pPEkt9Ps4DJBNZAHSZ/rvbOkj//K/tq', 'admin');
+CREATE TABLE `item`
+(
+       `id` int(11) NOT NULL AUTO_INCREMENT,
+       `title` varchar(255) NOT NULL,
+       `price` smallint(5) NOT NULL,
+       PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=utf8;
+
+INSERT INTO `item` (`id`, `title`, `price`)
+VALUES (1,	'stůl', 500),
+       (2,	'židle', 300),
+       (3,	'skříň', 1000);
+
+CREATE TABLE `order_item`
+(
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+         `order_id` int(11) NOT NULL,
+         `item_id` int(11) NOT NULL,
+         PRIMARY KEY (`id`),
+         KEY `order_id` (`order_id`),
+         KEY `item_id` (`item_id`),
+         CONSTRAINT `order_item_ibfk_11` FOREIGN KEY (`item_id`) REFERENCES `item` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+         CONSTRAINT `order_item_ibfk_10` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8;
+
+INSERT INTO `order_item` (`id`, `order_id`, `item_id`)
+VALUES (1,	1,	1),
+       (2,	1,	2),
+       (3,	1,	3);
